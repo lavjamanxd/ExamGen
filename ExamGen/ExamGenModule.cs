2303dbb7b5d24a4be19c6da171f5e6666b6d62ba
David Franko <lavjamanxd@gmail.com>
2017-12-16 12:35:42 +0100

    examgen added fill the questions.txt with question

---
+++ ExamGen/ExamGenModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Nancy;
+
+namespace ExamGen
+{
+    public sealed class ExamGenModule : NancyModule
+    {
+        public ExamGenModule()
+        {
+            Get("/",
+                args => string.Join(Environment.NewLine,
+                    GenerateRandomList(20, "questions.txt").Zip(Enumerable.Range(1, 20),
+                        (question, index) => index + ". " + question)));
+        }
+
+        public List<string> GenerateRandomList(int number, string filePath) => File.ReadAllLines(filePath).ToList()
+            .Where(x => !string.IsNullOrEmpty(x)).OrderBy(x => Guid.NewGuid()).Take(number).ToList();
+    }
+}
